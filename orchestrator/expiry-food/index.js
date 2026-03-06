76a956f5210f926d6ff1b1b6468003510556000c
Liang Ang Yee <x4yees@gmail.com>
2026-03-06 22:36:44 +0800

    Removed redundant files
    - Remove node_modules in main directory

--- orchestrator/expiry-food/index.js
+++ orchestrator/expiry-food/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const app = express();
+const axios = require('axios');
 
+const app = express();
 app.use(express.json());
 
-app.delete('/', (req, res) => {
+// inventory service address
+const INVENTORY_SERVICE = "http://localhost:8000";
+
+app.delete('/', async (req, res) => {
     console.log("Expiry orchestrator hit!");
-    
-    res.status(200).json({ 
-        success: true, 
-        message: "Expired food removal initiated from the orchestrator layer!" 
-    });
+
+    try {
+
+        // Call inventory service
+        const inventory = await axios.get(`${INVENTORY_SERVICE}/inventory`);
+
+        console.log("Inventory received:", inventory.data);
+
+        res.status(200).json({
+            success: true,
+            message: "Expired food removal initiated from the orchestrator layer!",
+            inventory: inventory.data
+        });
+
+    } catch (error) {
+
+        console.error("Error calling inventory service:", error.message);
+
+        res.status(500).json({
+            success: false,
+            error: "Failed to call inventory service"
+        });
+
+    }
 });
 
 app.listen(3000, '0.0.0.0', () => {
--- services/inventory-service/index.js
+++ services/inventory-service/index.js
@@ -1,16 +1,47 @@
-const express = require("express");
+import express from 'express'
+import {db} from './firebaseAdmin.js'
+
 const app = express();
 app.use(express.json());
 
-app.get("/inventory",(req,res)=>{
+const INVENTORY = db.collection('inventory')
+
+app.get("/inventory",async (req,res)=>{
   res.json([
     {food:"bread",qty:10},
     {food:"milk",qty:5}
   ])
+  // try {
+  //   const snapshot = await INVENTORY.get();
+
+  //   const todos = snapshot.docs.map(doc => ({
+  //     id: doc.id,
+  //     ...doc.data()
+  //   }));
+
+  //   res.json(todos);
+
+  // } catch (err) {
+  //   res.status(500).json({ error: err.message });
+  // }
 })
 
-app.post("/inventory/update",(req,res)=>{
-  res.json({message:"inventory updated"})
+app.get("/inventory/:id", async (req,res)=>{
+  try {
+    const doc = await INVENTORY.doc(req.params.id).get();
+
+    if (!doc.exists) {
+      return res.status(404).json({ error: "Not found" });
+    }
+
+    res.json({
+      id: doc.id,
+      ...doc.data()
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 })
 
 app.listen(3000)
